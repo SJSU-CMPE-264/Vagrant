2f859fa4cf75bc3139cdaf0e275c1e0e29c1703c
Alex Norell <alex.norell@gmail.com>
2018-12-04 05:10:46 -0800

    Removing Colormake. Updated verification

--- init.sh
+++ init.sh
@@ -2,8 +2,6 @@ echo "Setting up linked folder, turning on colored prompt, setting cocotb locati
 sed -i '/force_color_prompt=yes/s/^#//g' /home/vagrant/.bashrc
 ln -s /vagrant/code/ /home/vagrant
 ln -s /vagrant/verification /home/vagrant
-ln -s /vagrant/group-awesome /home/vagrant
-echo "alias make=colormake" >> /home/vagrant/.bashrc
 echo "export COCOTB=~/cocotb" >> /home/vagrant/.bashrc
 
 echo "getting cocotb from github repo"
